593640633ad3c12c80b5e2f0633585e131d8ae89
Guilherme <guilherme.cottas@portoseguro.com.br>
2024-09-17 18:48:20 -0300

    Implemetação da classe Monte com método atingirTopo

---
+++ src/br/com/impacta/exercicio4/Monte.java
@@ -0,0 +1,10 @@
+package br.com.impacta.exercicio4;
+
+public class Monte {
+	private double altura;
+	private String dificuldade;
+	
+	public void atingirTopo() {
+		System.out.println("Topo foi Atingido");
+	}
+}
