4daab05e49402369f8ee95b1f9abe320f5941b10
Guilherme <guilherme.cottas@portoseguro.com.br>
2024-09-17 18:46:11 -0300

    Implementação da Classe Pedra com método rolar

---
+++ src/br/com/impacta/exercicio4/Pedra.java
@@ -0,0 +1,11 @@
+package br.com.impacta.exercicio4;
+
+public class Pedra {
+	
+	private double peso;
+	
+	public void rolar() {
+		System.out.println("Rolando");
+	}
+	
+}
--- src/br/com/impacta/exercicio4/Sisifo.java
+++ src/br/com/impacta/exercicio4/Sisifo.java
@@ -3,6 +3,9 @@ package br.com.impacta.exercicio4;
 public class Sisifo extends Pessoa{
 	private double forca;
 	
-	
+	@Override
+	public void empurrarPedra() {
+		System.out.println("Empurrao repetitivo da Pedra");
+	}
 
 }
